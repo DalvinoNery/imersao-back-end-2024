c2f8b47825d8eafefdb2b29583bd7bd9043dcf6b
ddrsnery <dalvino.nery@solutis.com.br>
2024-11-21 13:58:40 -0300

    rota com upload de imagens

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,5 @@
-import getTodosPosts from "../models/postsModel.js";
-
+import fs from "fs";
+import {getTodosPosts, salvar} from "../models/postsModel.js";
 
 export async function listar(req, res) {
     // Chamando a função para buscar os posts
@@ -7,4 +7,35 @@ export async function listar(req, res) {
     // Enviando os posts como resposta em formato JSON com status 200 (sucesso)
     res.status(200).json(resultado);
 };
+
+export async function cadastrar(req, res) {
+    const novoPost = req.body;
+    try{
+       const postCriado = await  salvar(novoPost); 
+       res.status(200).json(postCriado);
+    }catch(erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"});
+    }
+    
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt:""
+    };
+    
+    try{
+       const postCriado = await  salvar(novoPost); 
+       const imagemAtualizada =`uploads/${postCriado.insertedId}.png`;
+       fs.renameSync(req.file.path, imagemAtualizada);
+       res.status(200).json(postCriado);
+    }catch(erro){
+        console.error(erro.message);
+        res.status(500).json({"Erro" : "Falha na requisição"});
+    }
+}    
+
 
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,12 +1,28 @@
 import express from "express";
-import { listar } from "../controllers/postsController.js";
+import multer from "multer";
+import {cadastrar, listar, uploadImagem} from "../controllers/postsController.js";
 
+//**Constante para manter o nome do arquivo que está sendo upado. Configuração no windows */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) =>{
     // **Middleware para interpretar JSON no corpo das requisições**
     app.use(express.json());
     // **Rota para buscar todos os posts**
     app.get("/posts", listar);
+    //**Rota para cadastrar um post**
+    app.post("/posts", cadastrar)
+    //**Rota para cadastrar posts com upload de imagem */
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 };
 
 export default routes;
